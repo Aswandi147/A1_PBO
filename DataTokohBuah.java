8cecbcb30ce91cd245ab2c1c0eb11d368aefd152
Aswandi147 <102606036+Aswandi147@users.noreply.github.com>
2022-04-19 22:58:28 +0800

    Aswandi_Posttest4_147

--- DataTokohBuah.java
+++ DataTokohBuah.java
@@ -1,99 +1,103 @@
-package Posttest2;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import static java.time.Clock.system;
+package Posttest3_Aswandi;
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class DataTokohBuah {
-    static ArrayList ListTokoBuah = new ArrayList();
-    static boolean isRunning = true;
-    static InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-    static BufferedReader input = new BufferedReader(inputStreamReader);
-    private static Object stockbuah;
-    private static int indexTokohBuah;
+     public static void main(String[] args){
+        ArrayList<TokoBuah> dataToko = new ArrayList<>();
 
-    static void ShowMenu() throws IOException{
-        System.out.println("======Menu=====");
-        System.out.println("[1] Show All Data Toko Buah");
-        System.out.println("[2] Tambah Data Toko Buah");
-        System.out.println("[3] Edit Data Toko Buah");
-        System.out.println("[4] Hapus Data Toko Buah");
-        System.out.println("[5] Exit");
-        System.out.println("Pilih menu>");
-        
-        int PilihMenu = Integer.valueOf(input.readLine());
-        switch(PilihMenu){
-            case 1:
-                showAllTokoBuah();
-                break;
-            case 2:
-                TambahTokoBuah();
-                break;
-            case 3:
-                EditTokoBuah();
-                break;
-            case 4:
-                HapusTokoBuah();
-                break;
-            case 5:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Pilihan anda tidak tersedia");
-        }
-    }
-        static void showAllTokoBuah(){
-            if(ListTokoBuah.isEmpty()){
-                System.out.println("Belum ada data");
-            } else {
-                for(int i = 0; i < ListTokoBuah.size(); i++){
-                    System.out.println(String.format("[%d] %s",i, ListTokoBuah.get(i)));
+        int pilih;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.println("||==Data Kebun BInatang==||");
+            System.out.println("[1] Lihat Data Kebun BInatang");
+            System.out.println("[2] Tambah Data kebun Binatang");
+            System.out.println("[3] Edit Data Kebun Binatang");
+            System.out.println("[4] Hapus Data Kebun Binatang");
+            System.out.println("[5] Exit ");
+            System.out.println("------------------------------");
+            System.out.println("Pilih menu yang ingin anda pilih");
+            pilih = scan.nextInt();
+            if (pilih == 1){
+                System.out.println("Menu Tampilkan Data Kebun Binatang");
+                if(dataToko.isEmpty()) System.out.println("Data Kosong!");
+                else {
+                    for(int i = 0; i < dataToko.size(); i++) {
+                        System.out.println("=======================================");
+                        System.out.println("Data ke-" + (i+1));
+                        System.out.println("Nama_Toko    : " + dataToko.get(i).getNama_Toko());
+                        System.out.println("Nama_Buah   : " + dataToko.get(i).getNama_Buah());
+                        System.out.println("Jumlah_StockBuah : " + dataToko.get(i).getJumlah_StockBuah());
+                        System.out.println("=======================================");
+                    }                    
                 }
-            }     
-        }
-        static void TambahTokoBuah() throws IOException{
-            System.out.println("Nama Toko Buah:");
-            System.out.println("Stock buah:");
-            System.out.println("Harga buah:");
-            String namatokobuah = input.readLine();
-            String Stockbuah = input.readLine();
-            String Hargabuah = input.readLine();
-            ListTokoBuah.add(namatokobuah);
-            ListTokoBuah.add(Stockbuah);
-            ListTokoBuah.add(Hargabuah);
-        }
-        static void EditTokoBuah() throws IOException{
-            showAllTokoBuah();
-            System.out.print("Pilih nomor Data Toko Buah:");
-            int indexTokoBuah = Integer.valueOf(input.readLine());
-            
-            System.out.println("Nama Toko Buah:");
-            System.out.println("Stock buah:");
-            System.out.println("Harga buah:");
-            String Stockbuah = input.readLine();
-            String Hargabuah = input.readLine();
-            String namaBaru = input.readLine();
-            
-            ListTokoBuah.set(indexTokoBuah, namaBaru);
-            ListTokoBuah.set(indexTokohBuah,Stockbuah);
-            ListTokoBuah.set(indexTokohBuah,Hargabuah);
-        }
-        static void HapusTokoBuah() throws IOException{
-            showAllTokoBuah();
-            System.out.print("Pilih Data Toko Buah Yang Ingin dihapus:");
-            int indexTokoBuah = Integer.valueOf(input.readLine());
-            
-            ListTokoBuah.remove(indexTokoBuah);
-        }
-        public static void main(String[] args)throws IOException{
-            do{
-                ShowMenu();
-            }while (isRunning);
-        }
+            }if (pilih == 2){
+                Scanner scancreate = new Scanner(System.in);
+                String Nama_Toko, Nama_Buah;
+                int jumlah_StockBuah;
+
+                System.out.println("Menu Tambah Data Toko");
+                System.out.print("Masukkan Nama_Toko    : ");
+                Nama_Toko = scancreate.nextLine();
+                System.out.print("Masukkan Nama_Buah   : ");
+                Nama_Buah = scancreate.nextLine();
+                System.out.print("Masukkan Jumlah_StockBuah : ");
+                jumlah_StockBuah = scancreate.nextInt();
+                dataToko.add(new TokoBuah (Nama_Toko, Nama_Buah, jumlah_StockBuah));
+                System.out.println("Data Berhasil Ditambahkan!");
+            }if (pilih == 3){
+                Scanner input = new Scanner(System.in);
+                Scanner scanupdate = new Scanner(System.in);
+                String nama_Toko,Nama_Buah;
+                int jumlah_StockBuah;
+        
+                System.out.println("Menu Ubah Data Kebun Binatang");
+                if(dataToko.isEmpty()) System.out.println("Data Kosong!");
+                else {
+                    for(int i = 0; i < dataToko.size(); i++) {
+                        System.out.println("=======================================");
+                        System.out.println("Data ke-" + (i+1));
+                        System.out.println("Nama_Toko    : " + dataToko.get(i).getNama_Toko());
+                        System.out.println("Nama_Buah   : " + dataToko.get(i).getNama_Buah());
+                        System.out.println("Jumlah_StockBuah  : " + dataToko.get(i).getJumlah_StockBuah());
+                        System.out.println("=======================================");
+                    }                    
+                }
+                System.out.print("Masukkan Nomor Data yang ingin diubah: ");
+                int no = input.nextInt();
+                System.out.print("Masukkan Nama : ");
+                nama_Toko = scanupdate.nextLine();
+                System.out.print("Masukkan Jenis : ");
+                Nama_Buah = scanupdate.nextLine();
+                System.out.print("Masukkan Jumlah : ");
+                jumlah_StockBuah = scanupdate.nextInt();
+                dataToko.set(no-1, new TokoBuah(nama_Toko, Nama_Buah,jumlah_StockBuah));
+                System.out.println("Data Berhasil Diubah!");                
+            }if (pilih == 4){
+                System.out.println("Menu Hapus Data Toko Buah");
+                Scanner input = new Scanner(System.in);
+        
+                System.out.println("Menu Ubah Data Toko Buah");
+                if(dataToko.isEmpty()) System.out.println("Data Kosong!");
+                else {
+                    for(int i = 0; i < dataToko.size(); i++) {
+                        System.out.println("=======================================");
+                        System.out.println("Data ke-" + (i+1));
+                        System.out.println("Nama_Buah    : " + dataToko.get(i).getNama_Toko());
+                        System.out.println("Nama_Buah   : " + dataToko.get(i).getNama_Buah());
+                        System.out.println("Habitat : " + dataToko.get(i).getJumlah_StockBuah());
+                        System.out.println("=======================================");
+                    }
+                }
+                System.out.print("Masukkan Nomor Data yang ingin dihapus: ");
+                int no = input.nextInt();
+                dataToko.remove(no-1);
+                System.out.println("Data Berhasil Dihapus!");
+            }if (pilih == 5){
+                System.out.println("Terima Kasih");
+            }
+        } while(pilih != 5);      
     }
-
-    
-
+}
 
 
