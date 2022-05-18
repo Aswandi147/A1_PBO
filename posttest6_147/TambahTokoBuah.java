f88b2dc84dedaab59030b770f0e79d58de1a350f
Aswandi147 <102606036+Aswandi147@users.noreply.github.com>
2022-05-18 16:00:32 +0800

    posttest6_147

---
+++ posttest6_147/TambahTokoBuah.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posttest6_147;
+
+public class TambahTokoBuah extends TokoBuah{
+    public TambahTokoBuah(String Nama_Toko, String Nama_Buah, int Jumlah_StockBuah){
+        super(Nama_Toko,Nama_Buah,Jumlah_StockBuah);
+    }
+}
+
---
+++ posttest6_147/Tampilan.java
@@ -0,0 +1,13 @@
+
+package posttest6_147;
+
+interface Tampilan {
+    public int displayProses();
+    public String getNama_Toko();
+    public void setNama_Toko(String Nama_Toko);
+    public String getNama_Buah();
+    public void setNama_Buah(String Nama_Buah);
+    public int getJumlah_StockBuah();
+    public void setJumlah_StockBuah(int Jumlah_StockBuah); 
+    public void setProses();
+}
